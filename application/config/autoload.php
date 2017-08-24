159657d266a48be96d1ddd2f4061d33883414646
Farrah <farrah.dionisio@astridtechnologies.com>
2017-08-24 14:15:08 -0700

    SASS stuffsxz

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session');
+$autoload['libraries'] = array('database','session','email');
 
 /*
 | -------------------------------------------------------------------
@@ -134,5 +134,5 @@ $autoload['language'] = array();
 */
 $autoload['model'] = array();
 
-/*Load CSS & JS */
+
 $autoload['helper'] = array('url');
