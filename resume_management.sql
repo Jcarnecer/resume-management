a3e9a2e21527ede3653b1228488a8bfbc7556804
farrahdeeee <farrahdee24@gmail.com>
2017-09-24 10:46:59 -0700

    updates on sql tables

--- resume_management.sql
+++ resume_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2017 at 01:06 AM
+-- Generation Time: Sep 24, 2017 at 07:46 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -75,70 +75,22 @@ INSERT INTO `applicants` (`id`, `first_name`, `last_name`, `middle_name`, `degre
 
 CREATE TABLE `employees` (
   `id` int(11) NOT NULL,
-  `last_name` varchar(100) DEFAULT NULL,
-  `first_name` varchar(100) DEFAULT NULL,
-  `middle_name` varchar(100) DEFAULT NULL,
-  `home_address` varchar(200) DEFAULT NULL,
-  `email_address` varchar(100) DEFAULT NULL,
-  `phone_number` varchar(50) DEFAULT NULL,
-  `birth_date` date DEFAULT NULL,
-  `position` varchar(100) DEFAULT NULL,
-  `date_hired` date DEFAULT NULL,
   `sss` varchar(100) DEFAULT NULL,
   `tin` varchar(100) DEFAULT NULL,
   `philhealth` varchar(100) DEFAULT NULL,
-  `pagibig` varchar(100) DEFAULT NULL,
-  `status` tinyint(4) DEFAULT NULL,
-  `employment_type` tinyint(4) NOT NULL,
-  `degree` varchar(100) DEFAULT NULL,
-  `school` varchar(100) DEFAULT NULL,
-  `comment` varchar(250) NOT NULL,
-  `role` int(11) NOT NULL
+  `pagibig` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`id`, `last_name`, `first_name`, `middle_name`, `home_address`, `email_address`, `phone_number`, `birth_date`, `position`, `date_hired`, `sss`, `tin`, `philhealth`, `pagibig`, `status`, `employment_type`, `degree`, `school`, `comment`, `role`) VALUES
-(26, 'Dionisio', 'Farrah', 'Delos Santos', '700 J.P Rizal St Makati City', 'Farrahdee24@gmail.com', '7291660', '0000-00-00', '23', '2017-09-21', NULL, NULL, NULL, NULL, 1, 3, 'BS Information Systems', 'University Of Santo Tomas', '', 0),
-(27, 'Dionisio', 'Farrah', 'Delos Santos', '700 J.P Rizal St Makati City', 'Farrahdee24@gmail.com', '7291660', '1996-09-24', '2', '0000-00-00', '', '', '', '', 0, 2, 'BS Information Systems', 'University Of Santo Tomas', '', 0),
-(28, 'Dionisio', 'Armani', 'Delos Santos', '700 J.P Rizal St Makati City', 'Farrahdee24@gmail.com', '7291660', '2017-09-24', '2', '0000-00-00', '', '', '', '', 0, 2, 'BS Information Systems', 'University Of Santo Tomas', '', 37);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `intern`
---
-
-CREATE TABLE `intern` (
-  `id` int(11) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `middle_name` varchar(100) NOT NULL,
-  `degree` varchar(150) NOT NULL,
-  `school` varchar(150) NOT NULL,
-  `application_status` tinyint(4) NOT NULL,
-  `application_date` date NOT NULL,
-  `expected_salary` varchar(50) NOT NULL,
-  `role` int(11) NOT NULL,
-  `home_address` varchar(250) NOT NULL,
-  `phone_number` varchar(100) NOT NULL,
-  `birth_date` date NOT NULL,
-  `date_hired` date NOT NULL,
-  `start_date` date NOT NULL,
-  `email_address` varchar(100) NOT NULL,
-  `file` varchar(250) NOT NULL,
-  `images` varchar(250) NOT NULL,
-  `available_date` date NOT NULL,
-  `interview_result` tinyint(4) NOT NULL,
-  `exam_result` tinyint(4) NOT NULL,
-  `interviewer` varchar(150) NOT NULL,
-  `interview_notes` varchar(200) NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `applicant_type` tinyint(4) NOT NULL,
-  `position` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `employees` (`id`, `sss`, `tin`, `philhealth`, `pagibig`) VALUES
+(26, NULL, NULL, NULL, NULL),
+(27, '', '', '', ''),
+(28, '', '', '', ''),
+(29, '', '', '', ''),
+(30, '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -156,9 +108,63 @@ CREATE TABLE `position` (
 --
 
 INSERT INTO `position` (`id`, `name`) VALUES
-(1, 'Applicant'),
-(2, 'Employee'),
-(3, 'Intern');
+(1, 'Employee'),
+(2, 'Intern');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record`
+--
+
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `middle_name` varchar(509) DEFAULT NULL,
+  `degree` varchar(100) DEFAULT NULL,
+  `application_status` tinyint(4) DEFAULT NULL,
+  `application_date` date DEFAULT NULL,
+  `role_id` varchar(50) DEFAULT NULL,
+  `pos_id` int(11) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `comment` text,
+  `home_address` text,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `date_hired` date DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `images` varchar(255) DEFAULT NULL,
+  `available_date` date DEFAULT NULL,
+  `interviewer` varchar(100) DEFAULT NULL,
+  `interview_note` text,
+  `current_status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `record`
+--
+
+INSERT INTO `record` (`id`, `first_name`, `last_name`, `middle_name`, `degree`, `application_status`, `application_date`, `role_id`, `pos_id`, `email`, `comment`, `home_address`, `phone_number`, `birthday`, `date_hired`, `file`, `images`, `available_date`, `interviewer`, `interview_note`, `current_status`) VALUES
+(1, 'Asdsadasdsad', 'Asdsad', 'Asdsad', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, NULL, '37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, NULL, '37', 1, 'asdsad@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, NULL, '37', 1, 'asdsad@gmail.com', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, NULL, '37', 1, 'asdsad@gmail.com', '', '', '22222', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(7, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, NULL, '37', 1, 'asdsad@gmail.com', '', '', '22222', '0000-00-00', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(8, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, NULL, '37', 1, 'asdsad@gmail.com', '', '', '22222', '0000-00-00', '0000-00-00', NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'Asdsadasdsad', 'Asdsad', 'Asdsad', '', NULL, '0000-00-00', '37', 1, 'asdsad@gmail.com', '', '', '22222', '0000-00-00', '0000-00-00', NULL, NULL, NULL, NULL, NULL, NULL),
+(10, 'Asdsadsad', 'Asdadsad', 'Asdsad', '', NULL, '0000-00-00', '36', 1, 'asdsadasd@gmail.com', '', '', '', '0000-00-00', '0000-00-00', NULL, NULL, NULL, NULL, NULL, NULL),
+(11, 'Asdsadsad', 'Asdadsad', 'Asdsad', '', NULL, '0000-00-00', '36', 1, 'asdsadasd@gmail.com', '', '', '', '0000-00-00', '0000-00-00', NULL, NULL, '0000-00-00', NULL, NULL, NULL),
+(12, 'Asdsadsad', 'Asdadsad', 'Asdsad', '', NULL, '0000-00-00', '36', 1, 'asdsadasd@gmail.com', '', '', '', '0000-00-00', '0000-00-00', NULL, NULL, '0000-00-00', NULL, NULL, 'Current'),
+(13, '', '', '', '', NULL, '0000-00-00', '', 0, '', '', '', '', '0000-00-00', '0000-00-00', NULL, NULL, '0000-00-00', NULL, NULL, 'Disabled Selected'),
+(14, 'Farrah', 'Dionisio', 'Delos Santos', 'BS Information Systems', NULL, '2017-06-19', '36', 1, 'farrahdee24@gmail.com', ';mf;salmfsal', 'Asd', '7291660', '1996-09-24', '0000-00-00', '20117372_1578945632155889_1325661114_n30.jpg', '21272215_1820654874641235_5372675952941739769_n4.jpg', '2017-06-19', NULL, NULL, 'Current'),
+(15, 'Farrah', 'Dionisio', 'Delos Santos', 'BS Information Systems', NULL, '2017-06-19', '36', 1, 'farrahdee24@gmail.com', 'Asca', '700 J.P Rizal St Makati City', '7291660', '1996-09-24', '0000-00-00', '20117372_1578945632155889_1325661114_n31.jpg', '20117372_1578945632155889_1325661114_n32.jpg', '2017-06-19', NULL, NULL, 'Applicant'),
+(16, 'Farrah', 'Dionisio', 'Delos Santos', 'BS Information Systems', NULL, '2017-06-19', '36', 1, 'farrahdee24@gmail.com', 'Asca', '700 J.P Rizal St Makati City', '7291660', '1996-09-24', '0000-00-00', '20117372_1578945632155889_1325661114_n33.jpg', '20117372_1578945632155889_1325661114_n34.jpg', '2017-06-19', NULL, NULL, 'Applicant'),
+(17, 'Farrah', 'Dionisio', 'Delos Santos', 'BS Information Systems', NULL, '2017-06-19', '36', 1, 'farrahdee24@gmail.com', 'Asca', '700 J.P Rizal St Makati City', '7291660', '1996-09-24', '0000-00-00', '20117372_1578945632155889_1325661114_n35.jpg', '20117372_1578945632155889_1325661114_n36.jpg', '2017-06-19', NULL, NULL, 'Applicant'),
+(18, 'Farrah', 'Dionisio', 'Delos Santos', 'BS Information Systems', NULL, '2017-06-19', '36', 1, 'farrahdee24@gmail.com', 'Asca', '700 J.P Rizal St Makati City', '7291660', '1996-09-24', '0000-00-00', '20117372_1578945632155889_1325661114_n37.jpg', '20117372_1578945632155889_1325661114_n38.jpg', '2017-06-19', NULL, NULL, 'Applicant'),
+(19, 'Armani', 'Dionisio', 'Delos Santos', 'BS Information Systems', NULL, '2017-06-19', '35', 2, 'farrahdee24@gmail.com', 'Sal,asl', '700 J.P Rizal St Makati City', '7291660', '1996-09-24', '0000-00-00', '20117372_1578945632155889_1325661114_n39.jpg', '20117372_1578945632155889_1325661114_n40.jpg', '2017-06-19', NULL, NULL, 'Disabled Selected');
 
 -- --------------------------------------------------------
 
@@ -169,17 +175,18 @@ INSERT INTO `position` (`id`, `name`) VALUES
 CREATE TABLE `role` (
   `role_id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
-  `pos_id` int(11) NOT NULL
+  `pos_id` int(11) NOT NULL,
+  `applicant` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `role`
 --
 
-INSERT INTO `role` (`role_id`, `name`, `pos_id`) VALUES
-(35, 'Java Developer', 3),
-(36, 'Front Developer', 1),
-(37, 'President', 2);
+INSERT INTO `role` (`role_id`, `name`, `pos_id`, `applicant`) VALUES
+(37, 'President', 1, 0),
+(38, 'Java Developer', 1, 0),
+(39, 'PHP Developer', 2, 0);
 
 --
 -- Indexes for dumped tables
@@ -198,15 +205,15 @@ ALTER TABLE `employees`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `intern`
---
-ALTER TABLE `intern`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `position`
 --
 ALTER TABLE `position`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `record`
+--
+ALTER TABLE `record`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -228,22 +235,22 @@ ALTER TABLE `applicants`
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
---
--- AUTO_INCREMENT for table `intern`
---
-ALTER TABLE `intern`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `position`
 --
 ALTER TABLE `position`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `record`
+--
+ALTER TABLE `record`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;COMMIT;
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
