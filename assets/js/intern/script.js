890a76727beaec9f6a1bdf86a0fd69686c44bab0
martslizardo <antoniomartin.lizardo@yahoo.com>
2017-10-27 10:39:20 +0800

    Fixed Js

--- assets/js/intern/script.js
+++ assets/js/intern/script.js
@@ -14,25 +14,25 @@ $(document).ready(function(){
           console.log(data);
           var result = JSON.parse(data);
           if(result=='success'){
-              $('[name="position"]').val('');
-              $('[name="role"]').val('');        
-              $('[name="last_name"]').val('');                
-              $('[name="first_name"]').val('');
-              $('[name="middle_name"]').val('');
-              $('[name="email_address"]').val('');       
-              $('[name="home_address"]').val('');       
-              $('[name="phone_number"]').val('');         
-              $('[name="birth_date"]').val('');       
-              $('[name="degree"]').val('');       
-              $('[name="school"]').val('');       
-              $('[name="application_date"]').val('');       
-              $('[name="available_date"]').val('');       
-              $('[name="expected_salary"]').val(''); 
-              $('[name="comment"]').val('');       
-              $('[name="resume_file"]').val('');
-              $('html, body').animate({ scrollTop: 0  }, "slow");
-              bs_notify("<strong>Successfully Added Record</strong>","success","top","right");         
-            //location.href=link;
+            //   $('[name="position"]').val('');
+            //   $('[name="role"]').val('');        
+            //   $('[name="last_name"]').val('');                
+            //   $('[name="first_name"]').val('');
+            //   $('[name="middle_name"]').val('');
+            //   $('[name="email_address"]').val('');       
+            //   $('[name="home_address"]').val('');       
+            //   $('[name="phone_number"]').val('');         
+            //   $('[name="birth_date"]').val('');       
+            //   $('[name="degree"]').val('');       
+            //   $('[name="school"]').val('');       
+            //   $('[name="application_date"]').val('');       
+            //   $('[name="available_date"]').val('');       
+            //   $('[name="expected_salary"]').val(''); 
+            //   $('[name="comment"]').val('');       
+            //   $('[name="resume_file"]').val('');
+            //   $('html, body').animate({ scrollTop: 0  }, "slow");
+            //   bs_notify("<strong>Successfully Added Record</strong>","success","top","right");         
+            location.href=link;
             }else{
                 bs_notify("<strong>"+result+"</strong>","danger","top","right");
             }
