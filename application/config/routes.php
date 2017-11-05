a658cdcd7c6cb20c350cbc58115d99f2ca92ca7a
martslizardo <antoniomartin.lizardo@yahoo.com>
2017-11-06 07:50:56 +0800

    Maintenance

--- application/config/routes.php
+++ application/config/routes.php
@@ -84,6 +84,6 @@ $route['roles/add']['POST']='roles/insert_role';
 $route['delete_role'] = 'Applicant/delete_role';
 
 
-$route['default_controller'] = "Home/index";
+$route['default_controller'] = "Home/maintenance";
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
