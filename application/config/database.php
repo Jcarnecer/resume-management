b60a01001777db5716d33e3979b5828b727c5d40
martslizardo <antoniomartin.lizardo@yahoo.com>
2017-11-20 09:50:47 +0800

    Fixed Login

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'   => '',
-	'hostname' =>'us-cdbr-iron-east-05.cleardb.net',
-	'username' => 'b1d5f8a10f4b0f',
-	'password' => '3cb2c655',
-	'database' => 'heroku_c9c99f1b3bb39cc',
+	'hostname' => 'us-cdbr-iron-east-05.cleardb.net',
+	'username' => 'bf5461f934a4a2',
+	'password' => 'edfca1c1',
+	'database' => 'heroku_aef913470229428',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
