a0e7fd5b9eacd3723be11346838c982da621e3f6
martslizardo <antoniomartin.lizardo@yahoo.com>
2017-12-01 14:56:17 +0800

    Edit Homepage

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -5,18 +5,18 @@
 </div>
   
   <div class="row">
-  <main role="main" class="col-xs-12 col-sm-10 col-md-10 col-lg-12 pt-3">
+  <main role="main" class="col-xs-12 col-sm-10 col-md-10 col-lg-10 pt-3">
   <!-- <a href="<?= base_url('roles')?>" class="btn custom-button" id="Roles"> View Roles</a> -->
     <div class="col-xs-12 col-sm-10 col-md-10 col-lg-10 center-block dashboard-margin">
       <ul class="button-group">
         <li><a href="<?= base_url('employee')?>" class="button-group-item active" id="employee">Employees</a></li>
         <li><a href="<?= base_url('Intern')?>" class="button-group-item" id="intern">Interns</a></li>
-        <li><a href="<?= base_url('freelance')?>" class="button-group-item" id="freelance">Freelancers</a></li>
         <li><a href="<?= base_url('applicant')?>" class="button-group-item" id="applicants">Applicants</a></li>
+        <li><a href="<?= base_url('freelance')?>" class="button-group-item" id="freelance">Freelancers</a></li>
       </ul>
     </div>
 
-    <h2>Interview Shcedules</h2>
+    <h2>Interview Schedules</h2>
           <div class="table-responsive">
             <table class="table table-striped">
               <thead>
@@ -26,27 +26,27 @@
                   <th>Time</th>
                   <th>Actions</th>
                 </tr>
-              </thead>
+              </thead> 
               <tbody>
                 <tr>
                   <td>Employee</td>
                   <td><?=$interview_employee==0?"None":$interview_employee?></td>
                   <td>9:00 am - 1:00pm</td>
-                  <td><button class="btn custom-button">View</button></td>
+                  <td>View</td>
                 
                 </tr>
                 <tr>
                   <td>Intern</td>
                   <td><?=$interview_intern==0?"None":$interview_intern?></td>
                   <td>9:00 am - 1:00pm</td>
-                  <td><button class="btn custom-button">View</button></td>
+                  <td>View</td>
                
                 </tr>
                 <tr>
                   <td>Freelance</td>
                   <td><?=$interview_freelance==0?"None":$interview_intern?></td>
                   <td>9:00 am - 1:00pm</td>
-                  <td><button class="btn custom-button">View</button></td>
+                  <td>View</td>
                 
                 </tr>
         </main>
